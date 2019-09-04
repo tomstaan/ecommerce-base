2e84e8bebc64a636fbec8e45f3338cd4adb3eef0
Tomasz Stankiewicz <36516273+TomSCoding@users.noreply.github.com>
2019-09-04 23:26:04 +0200

    Options Button Switch

--- ecom/frontend/src/components/products/Options.js
+++ ecom/frontend/src/components/products/Options.js
@@ -94,55 +94,7 @@ export class Options extends Component {
             <h2>Select All</h2>
           </div>
         </div>
-        <div className="filterBoxFrame">
-          <div className="filterBox" onClick={this.toggleFilter}>
-            <img src={Filter} alt="Filter" title="Filter" />
-            <div className="actionText">
-              <h2>Filter</h2>
-            </div>
-          </div>
-          {this.state.filterMenu ? (
-            <div className="filterBoxOptions">
-              <div className="filterSearchItem">
-                <input
-                  type="search"
-                  placeholder="Keyword"
-                  onChange={(e) => this.props.filterProducts(e.target.value)}
-                  value={this.props.filterValue}
-                />
-              </div>
-              <div className="filterSortTypes">
-                <div className="filterSortAsc">
-                  <div className="filterAscImg">
-                    <img src={Asc} alt="Ascending" title="Ascending" />
-                  </div>
-                </div>
-                <div className="filterSortDesc">
-                  <div className="filterDescImg">
-                    <img src={Desc} alt="Descending" title="Descending" />
-                  </div>
-                </div>
-              </div>
-              <div className="filterSortPrice">
-                <div className="filterSortPriceCover">
-                  <h2>Price</h2>
-                </div>
-              </div>
-              <div className="filterSortCategory">
-                <div className="filterSortCategoryCover">
-                  <h2>Category</h2>
-                </div>
-              </div>
-              <div className="filterSortUnits">
-                <div className="filterSortUnitsCover">
-                  <h2>Units Available</h2>
-                </div>
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
+
         <div className="actionBoxFrame">
           <div className="actionBox" onClick={this.toggleAction}>
             <img src={Actions} alt="Actions" title="Actions" />
@@ -168,6 +120,56 @@ export class Options extends Component {
           ) : (
             ""
           )}
+
+          <div className="filterBoxFrame">
+            <div className="filterBox" onClick={this.toggleFilter}>
+              <img src={Filter} alt="Filter" title="Filter" />
+              <div className="actionText">
+                <h2>Filter</h2>
+              </div>
+            </div>
+            {this.state.filterMenu ? (
+              <div className="filterBoxOptions">
+                <div className="filterSearchItem">
+                  <input
+                    type="search"
+                    placeholder="Keyword"
+                    onChange={e => this.props.filterProducts(e.target.value)}
+                    value={this.props.filterValue}
+                  />
+                </div>
+                <div className="filterSortTypes">
+                  <div className="filterSortAsc">
+                    <div className="filterAscImg">
+                      <img src={Asc} alt="Ascending" title="Ascending" />
+                    </div>
+                  </div>
+                  <div className="filterSortDesc">
+                    <div className="filterDescImg">
+                      <img src={Desc} alt="Descending" title="Descending" />
+                    </div>
+                  </div>
+                </div>
+                <div className="filterSortPrice">
+                  <div className="filterSortPriceCover">
+                    <h2>Price</h2>
+                  </div>
+                </div>
+                <div className="filterSortCategory">
+                  <div className="filterSortCategoryCover">
+                    <h2>Category</h2>
+                  </div>
+                </div>
+                <div className="filterSortUnits">
+                  <div className="filterSortUnitsCover">
+                    <h2>Units Available</h2>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
         <div className="AddProdBtn">
           <img src={Add} alt="Add" title="Add" />
