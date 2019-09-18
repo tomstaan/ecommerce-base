cd0d8a22e3063b1e4c7ec1b346375c1ce28f2878
Tomasz Stankiewicz <36516273+TomSCoding@users.noreply.github.com>
2019-09-18 12:40:21 +0100

    frontend products

--- ecom/frontend/src/components/products/NewProduct.js
+++ ecom/frontend/src/components/products/NewProduct.js
@@ -1,14 +1,134 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect, Provider } from "react-redux";
+import "./../style/newproduct.css";
 
+//Images
+import Arrow from "./../style/images/return.png";
+import GalleryIcon from "./../style/images/pic.png";
 
 export class NewProduct extends Component {
+  state = {
+    productName: "",
+    productCategory: "",
+    productPrice: "",
+    productQuantity: "",
+    productSize: "",
+    productColor: "",
+    productWeight: "",
+    productStock: "",
+    productOrder: "",
+    productDesc: ""
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    const {
+      productName,
+      productCategory,
+      productPrice,
+      productQuantity,
+      productSize,
+      productColor,
+      productWeight,
+      productStock,
+      productOrder,
+      productDesc
+    } = this.state;
+    const product = {
+      productName,
+      productCategory,
+      productPrice,
+      productQuantity,
+      productSize,
+      productColor,
+      productWeight,
+      productStock,
+      productOrder,
+      productDesc
+    };
+    //this.props.addTask(task);
+    this.setState({
+      productName: "",
+      productCategory: "",
+      productPrice: "",
+      productQuantity: "",
+      productSize: "",
+      productColor: "",
+      productWeight: "",
+      productStock: "",
+      productOrder: "",
+      productDesc: ""
+    });
+  };
+
   render() {
+    const {
+      productName,
+      productCategory,
+      productPrice,
+      productQuantity,
+      productSize,
+      productColor,
+      productWeight,
+      productStock,
+      productOrder,
+      productDesc
+    } = this.state;
     return (
-      <div>
-        <button>Back</button>
-        <h3>New Product</h3>
+      <div className="NewProdBack">
+        <div className="col-lg-12">
+          <div className="topRoutingBack">
+            <div className="routeCont">
+              <h3>Products</h3>
+              <div className="routeArrow">
+                <img src={Arrow} alt="arrow" title="arrow" />
+              </div>
+              <h3>New Product</h3>
+            </div>
+          </div>
+          <div className="newProductTitle">
+            <h3>New Product</h3>
+          </div>
+          <form onSubmit={this.onSubmit}>
+            <div className="newProdTopFields">
+              <div className="newProdField">
+                <input
+                  className="form-control"
+                  type="text"
+                  name="productName"
+                  id="label-title"
+                  placeholder="Product Name"
+                  onChange={this.onChange}
+                  value={productName}
+                />
+              </div>
+              <div className="newProdField">
+                <input
+                  className="form-control"
+                  type="text"
+                  name="productCategory"
+                  id="label-title"
+                  placeholder="Product Category"
+                  onChange={this.onChange}
+                  value={productCategory}
+                />
+              </div>
+              <div className="newProdImageUploadCont">
+                <div className="newProdImageUploadLabel">
+                  <div className="newProdLabelImg">
+                    <img src={GalleryIcon} alt="Gallery Icon" title="Gallry Icon"/>
+                  </div>
+                  <div className="newProdGalleryButton">
+                    <button>Upload Images</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
--- ecom/frontend/src/components/products/products.js
+++ ecom/frontend/src/components/products/products.js
@@ -6,18 +6,23 @@ import NewProduct from "./NewProduct";
 import Options from "./Options";
 import ProductList from "./ProductList";
 
+temp => {
+  /*return(<div className="productLimit">
+            <div className="col-lg-12">
+              <Options />
+            </div>
+          </div>
+          <ProductList />)*/
+}
+
 export default class Products extends Component {
   render() {
     return (
       <Router>
         <div>
-          <div className="productLimit">
-            <div className="col-lg-12">
-              <Options />
-            </div>
-          </div>
-          <Route path="" component={ProductList} />
-          <Route path="/newproduct" component={NewProduct} />
+          
+          <NewProduct/>
+          <Route path="newproduct" component={NewProduct} />
         </div>
       </Router>
     );
