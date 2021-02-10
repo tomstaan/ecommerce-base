6277d223425808da2c47556b368d9cec1ff56d40
tomscoding <stankiewicztommy82@gmail.com>
2021-02-10 00:17:44 +0000

    user login backend

--- ecom/accounts/serializers.py
+++ ecom/accounts/serializers.py
@@ -19,5 +19,15 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
         return user
-        
+
 # Login Serializer
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
+ 
