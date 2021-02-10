4728dfa2166c04177d2cb0d7bf2f8087bcb5d6a9
tomscoding <stankiewicztommy82@gmail.com>
2021-02-10 00:30:10 +0000

    retrieve user

--- ecom/accounts/api.py
+++ ecom/accounts/api.py
@@ -29,6 +29,13 @@ class LoginAPI(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
         })
-        
-# Get User API
 
+# Get User API
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
